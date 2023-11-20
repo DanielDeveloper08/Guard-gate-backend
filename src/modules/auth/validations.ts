41973db7d36ec06f6b339e2d6f2e057125717f82
José Domínguez P <jdominguez@viamatica.me>
2023-11-20 16:05:19 -0500

    refactor(auth): ♻️ Rename file to validations

---
+++ src/modules/auth/validations.ts
@@ -0,0 +1,46 @@
+import { Joi, schema, validate } from 'express-validation';
+import { GlobalValidations } from '../../shared/global-validations';
+
+export class AuthValidations {
+
+  constructor(
+    private readonly _validations = new GlobalValidations()
+  ) {}
+
+  login = () => {
+    const loginSchema: schema = {
+      body: Joi.object({
+        usuario: this._validations.validString('usuario', 255),
+        contrasenia: this._validations.validString('contrasenia', 255),
+      }),
+    };
+
+    return validate(loginSchema);
+  };
+
+  validateLogin = () => {
+    const validateLoginSchema: schema = {
+      body: Joi.object({
+        usuario: this._validations.validString('usuario', 255),
+        codigo: this._validations.validString('codigo', 8),
+      }),
+    };
+
+    return validate(validateLoginSchema);
+  };
+
+  register = () => {
+    const registerSchema: schema = {
+      body: Joi.object({
+        usuario: this._validations.validString('usuario', 255),
+        contrasenia: this._validations.validString('contrasenia', 255),
+        nombres: this._validations.validString('nombres', 255),
+        apellidos: this._validations.validString('apellidos', 255),
+        correo: this._validations.validString('correo', 255),
+        telefono: this._validations.validStringNoRequired('telefono', 255),
+      }),
+    };
+
+    return validate(registerSchema);
+  };
+}
