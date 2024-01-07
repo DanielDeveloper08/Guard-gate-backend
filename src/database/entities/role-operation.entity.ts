2d37e0d8db96783ebe79d190e65715d4b4a5079c
DEATHSHOOTERgd <giampaolo.delgadoo@ug.edu.ec>
2024-01-07 13:32:35 -0500

    Get role endpoint fix

--- src/database/entities/role-operation.entity.ts
+++ src/database/entities/role-operation.entity.ts
@@ -1,24 +1,21 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 import { OperationEntity } from './operation.entity';
 import { RoleEntity } from './role.entity';
+import { BaseEntity } from '../../shared/base-entity';
 
 @Entity({ name: 'rol_operacion' })
-export class RoleOperationEntity {
-
+export class RoleOperationEntity{
   @PrimaryGeneratedColumn('increment', { type: 'integer', name: 'id' })
   id!: number;
 
   @Column('integer', { name: 'id_rol' })
   roleId!: number;
-
   @Column('integer', { name: 'id_operacion' })
   operationId!: number;
 
-  @ManyToOne(() => RoleEntity, (role) => role.operations)
-  @JoinColumn({ name: 'id_rol', referencedColumnName: 'id' })
+  @ManyToOne(() => RoleEntity, (role) => role.roleOperations)
   rol!: RoleEntity;
 
-  @ManyToOne(() => OperationEntity, (operation) => operation.roles)
-  @JoinColumn({ name: 'id_operacion', referencedColumnName: 'id' })
+  @ManyToOne(() => OperationEntity, (operation) => operation.roleOperations)
   operation!: OperationEntity;
 }
