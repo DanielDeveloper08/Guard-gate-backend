13f564b70bf1804c51d3e993036fe957220ca278
José Domínguez P. <josemidev24@gmail.com>
2024-01-27 21:41:51 -0500

    refactor(role-operation): ♻️ Updated edit service

--- src/modules/operation/repository.ts
+++ src/modules/operation/repository.ts
@@ -12,4 +12,44 @@ export class OperationRepository {
   getAll(cnx: EntityManager) {
     return cnx.find(OperationEntity);
   }
+
+  async update(cnx: EntityManager, id: number, payload: OperationEntity) {
+    const update = await cnx.update(
+      OperationEntity,
+      { id },
+      payload
+    );
+
+    return update.affected;
+  }
+
+  async disableOperations(cnx: EntityManager, operationsIdxs: number[]) {
+    const update = await cnx
+      .createQueryBuilder()
+      .update(OperationEntity)
+      .set({
+        status: false,
+        updatedAt: new Date(),
+      })
+      .where('id NOT IN (:...idxs)', { idxs: operationsIdxs })
+      .andWhere('estado = true')
+      .execute();
+
+    return update.affected;
+  }
+
+  async resetOperations(cnx: EntityManager, operationsIdxs: number[]) {
+    const update = await cnx
+      .createQueryBuilder()
+      .update(OperationEntity)
+      .set({
+        status: false,
+        updatedAt: new Date(),
+      })
+      .where('id IN(:...idxs)', { idxs: operationsIdxs })
+      .andWhere('estado = true')
+      .execute();
+
+    return update.affected;
+  }
 }
--- src/modules/role-operation/repository.ts
+++ src/modules/role-operation/repository.ts
@@ -1,42 +1,16 @@
 import { EntityManager } from 'typeorm';
-import { RoleOperationEntity } from '../../database';
+import { OperationEntity, RoleOperationEntity } from '../../database';
 
 export class RoleOperationRepository {
 
-  getAnotherOperations(
-    cnx: EntityManager,
-    id: number,
-    operationsIds: number[]
-  ) {
+  getAllOpByRoleId(cnx: EntityManager, roleId: number) {
     const query = cnx
       .createQueryBuilder()
-      .select('id_operacion as id')
+      .select('role_op.id_operacion as id')
       .from(RoleOperationEntity, 'role_op')
-      .where('id_rol = :id', { id })
-      .andWhere('id_operacion NOT IN (:...ids)', { ids: operationsIds });
+      .where('role_op.id_rol = :roleId', { roleId });
 
     return query.getRawMany<Record<'id', number>>();
-  }
-
-  async resetOperation(
-    cnx: EntityManager,
-    id: number,
-    operationsIds: number[]
-  ) {
-    const update = await cnx
-      .createQueryBuilder()
-      .delete()
-      .from(RoleOperationEntity)
-      .where('id_rol = :id', { id })
-      .andWhere('id_operacion NOT IN (:...ids)', { ids: operationsIds })
-      .execute();
-
-    return update.affected;
-  }
-
-  async removeOpByRoleId(cnx: EntityManager, roleId: number) {
-    const remove = await cnx.delete(RoleOperationEntity, { roleId });
-    return remove.affected;
   }
 
   getOperation(cnx: EntityManager, roleId: number, operationId: number) {
@@ -48,8 +22,20 @@ export class RoleOperationRepository {
     });
   }
 
-  addOperation(cnx: EntityManager, payload: RoleOperationEntity) {
-    const insert = cnx.create(RoleOperationEntity, payload);
-    return cnx.save(insert);
+  getValidOperation(
+    cnx: EntityManager,
+    roleId: number,
+    operationId: number
+  ) {
+    const query = cnx
+      .createQueryBuilder()
+      .select('op.id as id')
+      .from(RoleOperationEntity, 'role_op')
+      .leftJoin(OperationEntity, 'op', 'role_op.id_operacion = op.id')
+      .where('role_op.id_rol = :roleId', { roleId })
+      .andWhere('op.id = :operationId', { operationId })
+      .andWhere('op.estado = true');
+
+    return query.getRawOne<Record<'id', number>>();
   }
 }
